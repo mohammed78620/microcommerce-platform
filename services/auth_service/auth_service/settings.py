dda537981f8ee84b17f93df0d47a4ec6087ab946
Mohammed Akmal Miah <mohammedmiah67890@gmail.com>
2026-05-19 17:46:04 +0100

    ENG-6 add frontend domain to allowed cors origins

--- services/auth_service/auth_service/settings.py
+++ services/auth_service/auth_service/settings.py
@@ -48,14 +48,19 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://d1d0aw4rn6ymob.cloudfront.net",
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = "auth_service.urls"
